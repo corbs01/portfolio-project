7da2b41edacde7850f1f2a556dd3b2733e1496fc
Corbin Ashby <corbskypow@gmail.com>
2020-08-23 18:33:06 +1200

    Add blog pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allblogs, name='home'),
+    path('', views.allblogs, name='bloghome'),
     path('<int:blog_id>/', views.detail, name='detail'),
 
 ]
